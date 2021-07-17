abcde28811a00295542c19c4153ea16f83565747
Ryan <jesselockard99@gmail.com>
2021-07-17 12:45:50 -0400

    enabled support for GraphQL Envelop + Helix

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -5,15 +5,19 @@ import {
 } from '@redwoodjs/api'
 
 import schemas from 'src/graphql/**/*.{js,ts}'
-import { db } from 'src/lib/db'
-import { logger } from 'src/lib/logger'
-import services from 'src/services/**/*.{js,ts}'
 
 import { getCurrentUser } from 'src/lib/auth'
+import { db } from 'src/lib/db'
+import { logger } from 'src/lib/logger'
+
+import services from 'src/services/**/*.{js,ts}'
 
 export const handler = createGraphQLHandler({
-  loggerConfig: { logger, options: {} },
   getCurrentUser,
+  loggerConfig: {
+    logger,
+    options: {},
+  },
   schema: makeMergedSchema({
     schemas,
     services: makeServices({ services }),
