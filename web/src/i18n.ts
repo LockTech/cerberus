ddef5c88399b36174c13e353464193fce01ffbcd
Ryan <jesselockard99@gmail.com>
2021-07-17 01:45:01 -0400

    reorganized locales

--- web/src/i18n.ts
+++ web/src/i18n.ts
@@ -2,7 +2,8 @@ import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
-import en from './locales/en.json'
+import en_app from './locales/en/app.json'
+import en_permissions from './locales/en/permissions.json'
 
 i18n
   .use(initReactI18next)
@@ -11,9 +12,12 @@ i18n
     interpolation: { escapeValue: false }, // React already does escaping
     lng: 'en',
     fallbackLng: 'en',
+    ns: ['app', 'permissions'],
+    defaultNS: 'app',
     resources: {
       en: {
-        translation: en,
+        app: en_app,
+        permissions: en_permissions,
       },
     },
   })
