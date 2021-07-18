8ac868fe971a07090bf0a0e60fcf218700f357b9
Ryan <jesselockard99@gmail.com>
2021-07-17 22:51:11 -0400

    wrapped Login and SignupPages in AuthLayout

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,19 +1,14 @@
-// In this file, all Page components from 'src/pages` are auto-imported. Nested
-// directories are supported, and should be uppercase. Each subdirectory will be
-// prepended onto the component name.
-//
-// Examples:
-//
-// 'src/pages/HomePage/HomePage.js'         -> HomePage
-// 'src/pages/Admin/BooksPage/BooksPage.js' -> AdminBooksPage
+import { Router, Route, Set } from '@redwoodjs/router'
 
-import { Router, Route } from '@redwoodjs/router'
+import AuthLayout from 'src/layouts/AuthLayout'
 
 const Routes = () => {
   return (
     <Router>
-      <Route path="/login" page={LoginPage} name="login" />
-      <Route path="/signup" page={SignupPage} name="signup" />
+      <Set wrap={[AuthLayout]}>
+        <Route path="/login" page={LoginPage} name="login" />
+        <Route path="/signup" page={SignupPage} name="signup" />
+      </Set>
       <Route notfound page={NotFoundPage} />
     </Router>
   )
