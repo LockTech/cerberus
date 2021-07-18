daf8e83832cdbfa3a18027c5230de069c7e020c2
Ryan <jesselockard99@gmail.com>
2021-07-17 22:50:15 -0400

    created AuthLayout component

---
+++ web/src/layouts/AuthLayout/AuthLayout.tsx
@@ -0,0 +1,11 @@
+import './AuthLayout.css'
+
+type AuthLayoutProps = {
+  children: React.ReactNode
+}
+
+const AuthLayout = ({ children }: AuthLayoutProps) => {
+  return <div className="auth-layout">{children}</div>
+}
+
+export default AuthLayout
