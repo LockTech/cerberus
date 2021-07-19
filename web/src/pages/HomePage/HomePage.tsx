abe1abd36598dd92e645b3a16d7aa3dfc5524ba0
Ryan <jesselockard99@gmail.com>
2021-07-18 20:51:39 -0400

    added HomePage Helmet locale

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,17 +1,26 @@
+import { useTranslation } from 'react-i18next'
 import { Link, routes } from '@redwoodjs/router'
+import { Helmet } from '@redwoodjs/web'
 
 const HomePage = () => {
+  const { t } = useTranslation()
+
   return (
-    <div className="text">
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
-    </div>
+    <>
+      <Helmet>
+        <title>{t('Home.Page.Helmet.title')}</title>
+      </Helmet>
+      <div className="text">
+        <h1>HomePage</h1>
+        <p>
+          Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
+        </p>
+        <p>
+          My default route is named <code>home</code>, link to me with `
+          <Link to={routes.home()}>Home</Link>`
+        </p>
+      </div>
+    </>
   )
 }
 
