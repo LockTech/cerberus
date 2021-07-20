d1712d6b029c215710689937d41b89900ca7e8d7
Ryan <jesselockard99@gmail.com>
2021-07-19 20:26:08 -0400

    impl DashboardLayout

--- web/src/layouts/DashboardLayout/DashboardLayout.tsx
+++ web/src/layouts/DashboardLayout/DashboardLayout.tsx
@@ -1,9 +1,35 @@
+import clsx from 'clsx'
+import { useTranslation } from 'react-i18next'
+import { routes, useMatch } from '@redwoodjs/router'
+
+import Appbar from 'src/components/Appbar'
+
 type DashboardLayoutProps = {
   children: React.ReactNode
 }
 
 const DashboardLayout = ({ children }: DashboardLayoutProps) => {
-  return <>{children}</>
+  const { t } = useTranslation()
+
+  return (
+    <div className="flex flex-row h-full">
+      <div className="sidebar">
+        <div className="site-title">
+          <h1>{t('Dashboard.Layout.sidebar.appname')}</h1>
+        </div>
+        <nav>
+          <button className={clsx(useMatch(routes.home()) && 'active')}>
+            {t('Dashboard.Layout.sidebar.links.home')}
+          </button>
+          <button>An Example</button>
+        </nav>
+      </div>
+      <div className="flex flex-col w-full">
+        <Appbar />
+        <main className="p-9">{children}</main>
+      </div>
+    </div>
+  )
 }
 
 export default DashboardLayout
