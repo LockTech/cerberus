94ddf202985e21aa12bc3de9c6617f0166cc443e
Ryan <jesselockard99@gmail.com>
2021-07-20 23:29:30 -0400

    updated Appbar

--- web/src/components/Appbar/Appbar.tsx
+++ web/src/components/Appbar/Appbar.tsx
@@ -1,16 +1,29 @@
-// import { useTranslation } from 'react-i18next'
-import { UserCircleIcon } from '@heroicons/react/outline'
+import { useTranslation } from 'react-i18next'
+import { QuestionMarkCircleIcon, UserCircleIcon } from '@heroicons/react/solid'
 
 import './Appbar.css'
 
 const Appbar = () => {
-  // const { t } = useTranslation()
+  const { t } = useTranslation()
 
   return (
     <div className="appbar">
       <div className="actions">
         <button>
-          <UserCircleIcon className="icon" />
+          <abbr title={t('Appbar.actions.manual')}>
+            <QuestionMarkCircleIcon
+              aria-label={t('Appbar.actions.manual')}
+              className="icon"
+            />
+          </abbr>
+        </button>
+        <button>
+          <abbr title={t('Appbar.actions.userProfile')}>
+            <UserCircleIcon
+              aria-label={t('Appbar.actions.userProfile')}
+              className="icon"
+            />
+          </abbr>
         </button>
       </div>
     </div>
