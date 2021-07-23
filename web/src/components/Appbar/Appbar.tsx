a56f8b4698329a85dd0603488b28fb3a023f47ec
Ryan <jesselockard99@gmail.com>
2021-07-23 15:11:08 -0400

    impl AppbarTitleAtom on Appbar, updated styling

--- web/src/components/Appbar/Appbar.tsx
+++ web/src/components/Appbar/Appbar.tsx
@@ -1,13 +1,19 @@
 import { useTranslation } from 'react-i18next'
+import { useRecoilValue } from 'recoil'
 import { UserCircleIcon } from '@heroicons/react/solid'
+
+import { AppbarTitleAtom } from 'src/atoms/AppbarTitleAtom'
 
 import './Appbar.css'
 
 const Appbar = () => {
   const { t } = useTranslation()
 
+  const title = useRecoilValue(AppbarTitleAtom)
+
   return (
     <div className="appbar">
+      {title !== '' && <h2>{title}</h2>}
       <div className="actions">
         {/* <button>
           <abbr title={t('Appbar.actions.manual')}>
