776ee6266cf2d95a5102ce15ecd0cfc59b9292ef
Ryan <jesselockard99@gmail.com>
2021-07-23 15:11:22 -0400

    impl AppbarTitle on HomePage, removed styling

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,8 @@
 import { useTranslation } from 'react-i18next'
 import { Helmet } from '@redwoodjs/web'
 
+import AppbarTitle from 'src/components/Appbar/AppbarTitle'
+
 import './HomePage.css'
 
 const HomePage = () => {
@@ -11,10 +13,7 @@ const HomePage = () => {
       <Helmet>
         <title>{t('Home.Page.Helmet.title')}</title>
       </Helmet>
-      <div className="title-group">
-        <h2 className="title">{t('Home.Page.title')}</h2>
-        <p className="hint">{t('Home.Page.subtitle')}</p>
-      </div>
+      <AppbarTitle>{t('Home.Page.Appbar.title')}</AppbarTitle>
     </>
   )
 }
