eb308bc0078671942d94a40a5c3d899a5ca167e8
Ryan <jesselockard99@gmail.com>
2021-07-24 04:05:49 -0400

    updated collectCoverageFrom

--- api/jest.config.js
+++ api/jest.config.js
@@ -3,5 +3,9 @@ const redwoodConfig = require('@redwoodjs/testing/config/jest/api')
 module.exports = {
   ...redwoodConfig,
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts', '!src/graphql/*.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/functions/*.ts',
+    '!src/graphql/*.ts',
+  ],
 }
