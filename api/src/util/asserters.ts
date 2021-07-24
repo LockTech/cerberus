035c1cd914d7f303b691687ca347828a790b2876
Ryan <jesselockard99@gmail.com>
2021-07-24 04:01:48 -0400

    removed unused asserter

--- api/src/util/asserters.ts
+++ api/src/util/asserters.ts
@@ -1,8 +1,3 @@
-export const isBool = (value: unknown) => {
-  if (typeof value === 'boolean') return true
-  else return false
-}
-
 export const isStr = (value: unknown) => {
   if (typeof value === 'string') return true
   else return false
