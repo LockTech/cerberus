1daa47b0fdd6c159c08db32d4a21c4a52c2c5608
Ryan <jesselockard99@gmail.com>
2021-07-24 02:09:18 -0400

    created checkEmailTaken helper, impl on signup and invite endpoints

--- api/src/services/accounts/accounts.ts
+++ api/src/services/accounts/accounts.ts
@@ -5,7 +5,6 @@ import { getCurrentUser } from 'src/lib/currentUser'
 import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
 
-import { isStr } from 'src/util/asserters'
 import { randomStr } from 'src/util/randomStr'
 
 import { validateEmail } from 'src/validators/email'
@@ -19,29 +18,30 @@ export const beforeResolver = (rules: BeforeResolverSpecType) => {
 //
 
 // ==
+const checkEmailTaken = async (service: string, email: string) => {
+  const accountExist = await db.account.count({ where: { email } })
+  if (accountExist !== 0) {
+    logger.warn(`[${service}]: Attempted to use taken email.`)
+    throw new SyntaxError('taken')
+  }
+}
+
 export interface SignupAccountArgs {
   email: string
 }
 export const signupAccount = async ({ email }: SignupAccountArgs) => {
-  if (!isStr(email)) {
-    logger.warn('Did not recieve a valid email for signupAccount.')
-    throw new SyntaxError('Cannot signup an account without an email address.')
-  }
-
-  const accountExist = await db.account.count({ where: { email } })
-  if (accountExist !== 0) {
-    logger.warn('Attempt to signup using a taken email address.')
-    throw new SyntaxError('Cannot signup using an already taken email address.')
-  }
+  await checkEmailTaken('signupAccount', email)
 
   const code = randomStr(6)
 
-  db.account_Confirmation.create({
+  await db.account_Confirmation.create({
     data: {
       code,
       email,
     },
   })
+
+  // send confirmation email
 
   return true
 }
@@ -50,28 +50,20 @@ export interface InviteMemberArgs {
   email: string
 }
 export const inviteMember = async ({ email }: InviteMemberArgs) => {
+  await checkEmailTaken('inviteMember', email)
+
   const organizationId = getCurrentUser().organizationId
-
-  const accountExist = await db.account.count({ where: { email } })
-  if (accountExist !== 0) {
-    logger.warn('Attempt to invite using a taken email address.')
-    throw new SyntaxError('Cannot invite using an already taken email address.')
-  }
-
-  if (!isStr(email)) {
-    logger.warn('Did not recieve a valid email for inviteMember.')
-    throw new SyntaxError('Cannot invite an account without an email address.')
-  }
-
   const code = randomStr(6)
 
-  db.account_Confirmation.create({
+  await db.account_Confirmation.create({
     data: {
       code,
       email,
       organizationId,
     },
   })
+
+  // send confirmation email
 
   return true
 }
