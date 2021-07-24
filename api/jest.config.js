7a959836b06a67014288505af9dab537eb0ac210
Ryan <jesselockard99@gmail.com>
2021-07-24 04:01:32 -0400

    enabled covage in API jest config, added coverage directory to .gitignore

--- api/jest.config.js
+++ api/jest.config.js
@@ -1,1 +1,7 @@
-module.exports = require('@redwoodjs/testing/config/jest/api')
+const redwoodConfig = require('@redwoodjs/testing/config/jest/api')
+
+module.exports = {
+  ...redwoodConfig,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts', '!src/graphql/*.ts'],
+}
