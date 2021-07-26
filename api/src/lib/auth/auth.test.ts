cca6806e5c5b17e6eabf6072a8e27a7ade4b6db3
Ryan <jesselockard99@gmail.com>
2021-07-25 21:21:23 -0400

    removed requireCurrentUser from auth lib

--- api/src/lib/auth/auth.test.ts
+++ api/src/lib/auth/auth.test.ts
@@ -1,16 +1,11 @@
 import type { Account } from '@prisma/client'
 
-import { getCurrentUser, requireCurrentUser } from './auth'
+import { getCurrentUser } from './auth'
 
 import type { AuthScenario } from './auth.scenarios'
 
-const AuthError: Error = {
-  name: 'AuthenticationError',
-  message: 'authentication',
-}
-
 describe('auth lib', () => {
-  describe('getCurrentUser', () => {
+  describe('getContextUser', () => {
     scenario(
       'successfully creates an account',
       async (scenario: AuthScenario) => {
@@ -20,30 +15,6 @@ describe('auth lib', () => {
         const res = await getCurrentUser({ id })
 
         expect(res).toEqual(expect.objectContaining<Account>(one))
-      }
-    )
-  })
-
-  describe('requireCurrentUser', () => {
-    scenario(
-      "throws an AuthenticationError when current request's user is undefined",
-      async (_scenario: AuthScenario) => {
-        mockCurrentUser(undefined)
-
-        expect(() => {
-          requireCurrentUser()
-        }).toThrow(AuthError)
-      }
-    )
-
-    scenario(
-      "throws an AuthenticationError when current request's user is null",
-      async (_scenario: AuthScenario) => {
-        mockCurrentUser(null)
-
-        expect(() => {
-          requireCurrentUser()
-        }).toThrow(AuthError)
       }
     )
   })
