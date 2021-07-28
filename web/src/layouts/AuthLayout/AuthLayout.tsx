84706c29b4f744519687dc0e37a50a28198dac65
Ryan <jesselockard99@gmail.com>
2021-07-27 23:47:51 -0400

    created BrandBanner component

--- web/src/layouts/AuthLayout/AuthLayout.tsx
+++ web/src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,3 +1,5 @@
+import BrandBanner from 'src/components/BrandBanner'
+
 import './AuthLayout.css'
 
 type AuthLayoutProps = {
@@ -5,7 +7,12 @@ type AuthLayoutProps = {
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
-  return <div className="auth-layout">{children}</div>
+  return (
+    <div className="auth-layout">
+      <BrandBanner />
+      {children}
+    </div>
+  )
 }
 
 export default AuthLayout
