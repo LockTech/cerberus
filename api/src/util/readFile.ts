c560a267b3b1f48878f36116473924ae19497ff9
Ryan <jesselockard99@gmail.com>
2021-07-27 23:09:08 -0400

    documented util files

--- api/src/util/readFile.ts
+++ api/src/util/readFile.ts
@@ -1,5 +1,14 @@
 import { readFileSync } from 'fs'
 
-export const readFile = (filePath: string) => {
-  return readFileSync(filePath).toString()
+import { isStr } from 'src/util/asserters'
+
+/**
+ * Read a file at `path`, converting the results to a `string`.
+ */
+export const readFile = (path: string) => {
+  if (!isStr(path)) {
+    throw new Error('Invalid file path.')
+  }
+
+  return readFileSync(path).toString()
 }
