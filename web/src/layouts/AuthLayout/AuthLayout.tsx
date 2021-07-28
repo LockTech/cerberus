88ac19e8f2f6c057d108860b25a813976ef6e2c1
Ryan <jesselockard99@gmail.com>
2021-07-28 01:46:24 -0400

    added privacy link to AuthLayout

--- web/src/layouts/AuthLayout/AuthLayout.tsx
+++ web/src/layouts/AuthLayout/AuthLayout.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next'
+
 import BrandBanner from 'src/components/BrandBanner'
 
 import './AuthLayout.css'
@@ -7,10 +9,17 @@ type AuthLayoutProps = {
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const { t } = useTranslation()
+
   return (
     <div className="auth-layout">
       <BrandBanner />
       {children}
+      <div className="auth-link">
+        <a className="link-primary" href="https://locktech.software/privacy">
+          {t('Auth.Layout.privacy')}
+        </a>
+      </div>
     </div>
   )
 }
