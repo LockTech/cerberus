9d2f1a4e5b311dad0d21cbcd91b9a086ede019a7
Ryan <jesselockard99@gmail.com>
2021-07-27 23:09:21 -0400

    added documentation to context and smtp lib files

--- api/src/lib/context.ts
+++ api/src/lib/context.ts
@@ -1,6 +1,10 @@
 import type { Account } from '@prisma/client'
 import { context } from '@redwoodjs/api'
 
+/**
+ * Retrieve the current request's authenticated `Account`;
+ * accepted as the account which initiated the request.
+ */
 export const getContextUser = () => {
   return context.currentUser as Account
 }
--- api/src/lib/smtp.ts
+++ api/src/lib/smtp.ts
@@ -5,8 +5,8 @@ import { logger } from 'src/lib/logger'
 
 const host = process.env.SMTP_HOST
 const port = parseInt(process.env.SMTP_PORT)
-const user = process.env.SMTP_USER
-const pass = process.env.SMTP_PASS
+const user = process.env.SMTP_USER || ''
+const pass = process.env.SMTP_PASS || ''
 const debug = process.env.SMTP_DEBUG === 'true' ? true : false
 
 const options: Options = {
@@ -21,4 +21,13 @@ const options: Options = {
   debug,
 }
 
+/**
+ * An SMTP `transporter`; can be used to send emails using any of the options available
+ * with [Nodemailer's `SMTPTransporter`](https://nodemailer.com/smtp/).
+ *
+ * An SMTP server should be configured using the provided environment variables.
+ *
+ * **Note:** For simple emailing, consider using `sendMail`. This function can be combined
+ * with `template` to easily created user generated emails.
+ */
 export const transporter = createTransport(options)
