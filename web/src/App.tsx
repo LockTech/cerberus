67d0f34c0aab90cbdd9abbb55f79ebc8f440e6cc
Ryan <jesselockard99@gmail.com>
2021-07-28 22:10:16 -0400

    updated default duration of all toast notifications

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -20,6 +20,7 @@ const App = () => {
               <Toaster
                 toastOptions={{
                   className: 'toast',
+                  duration: 3000,
                   error: {
                     iconTheme: {
                       primary: '#fb7185',
