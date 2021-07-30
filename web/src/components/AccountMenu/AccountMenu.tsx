1cc627796b28e96dc582701baa4884279d8b9c30
Ryan <jesselockard99@gmail.com>
2021-07-30 03:42:54 -0400

    added CurrentAccountSettingsModal to AccountMenu

--- web/src/components/AccountMenu/AccountMenu.tsx
+++ web/src/components/AccountMenu/AccountMenu.tsx
@@ -1,10 +1,13 @@
 import { useTranslation } from 'react-i18next'
-import { useRecoilValue } from 'recoil'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
 import { Menu, Transition } from '@headlessui/react'
 import { CogIcon, LogoutIcon, UserCircleIcon } from '@heroicons/react/solid'
 import { useAuth } from '@redwoodjs/auth'
 
 import { CurrentAccountAtom } from 'src/atoms/CurrentAccount'
+import { CurrentAccountSettingsModalAtom } from 'src/atoms/CurrentAccountSettingsModal'
+
+import CurrentAccountSettingsModal from 'src/components/CurrentAccountSettingsModal'
 
 import './AccountMenu.css'
 
@@ -12,45 +15,55 @@ const AccountMenu = () => {
   const { t } = useTranslation()
 
   const currentAccount = useRecoilValue(CurrentAccountAtom)
+  const setSettingsModalOpen = useSetRecoilState(
+    CurrentAccountSettingsModalAtom
+  )
 
   const { logOut } = useAuth()
 
   return (
-    <Menu as="div" className="account-menu">
-      <Menu.Button as="button" className="menu-button">
-        {currentAccount && (
-          <span className="title">{`${currentAccount.firstName} ${currentAccount.lastName}`}</span>
-        )}
-        <UserCircleIcon
-          aria-label={t('AccountMenu.accountProfile')}
-          className="icon"
-        />
-      </Menu.Button>
-      <Transition
-        as={React.Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="menu-items">
-          <Menu.Item as="button" className="menu-item">
-            <CogIcon aria-hidden="true" className="icon" />
-            {t('AccountMenu.items.settings')}
-          </Menu.Item>
-          <Menu.Item
-            as="button"
-            className="menu-item logout"
-            onClick={() => logOut()}
-          >
-            <LogoutIcon aria-hidden="true" className="icon" />
-            {t('AccountMenu.items.logout')}
-          </Menu.Item>
-        </Menu.Items>
-      </Transition>
-    </Menu>
+    <>
+      <CurrentAccountSettingsModal />
+      <Menu as="div" className="account-menu">
+        <Menu.Button as="button" className="menu-button">
+          {currentAccount && (
+            <span className="title">{`${currentAccount.firstName} ${currentAccount.lastName}`}</span>
+          )}
+          <UserCircleIcon
+            aria-label={t('AccountMenu.accountProfile')}
+            className="icon"
+          />
+        </Menu.Button>
+        <Transition
+          as={React.Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="menu-items">
+            <Menu.Item
+              as="button"
+              className="menu-item"
+              onClick={() => setSettingsModalOpen(true)}
+            >
+              <CogIcon aria-hidden="true" className="icon" />
+              {t('AccountMenu.items.settings')}
+            </Menu.Item>
+            <Menu.Item
+              as="button"
+              className="menu-item logout"
+              onClick={() => logOut()}
+            >
+              <LogoutIcon aria-hidden="true" className="icon" />
+              {t('AccountMenu.items.logout')}
+            </Menu.Item>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </>
   )
 }
 
