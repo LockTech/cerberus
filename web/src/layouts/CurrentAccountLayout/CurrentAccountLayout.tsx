67b129e9ac06dc379e9ceb76894e5297c1abab6d
Ryan <jesselockard99@gmail.com>
2021-08-01 20:19:26 -0400

    added loading indicator to CurrentAccountLayout

--- web/src/layouts/CurrentAccountLayout/CurrentAccountLayout.tsx
+++ web/src/layouts/CurrentAccountLayout/CurrentAccountLayout.tsx
@@ -1,12 +1,33 @@
-import { useQueryCurrentAccount } from 'src/hooks/useCurrentAccount'
+import { useTranslation } from 'react-i18next'
+
+import LoadingSpinner from 'src/components/LoadingSpinner/LoadingSpinner'
+
+import {
+  useCurrentAccount,
+  useQueryCurrentAccount,
+} from 'src/hooks/useCurrentAccount'
+
+import './CurrentAccountLayout.css'
 
 type CurrentAccountLayoutProps = {
   children?: React.ReactNode
 }
 
 const CurrentAccountLayout = ({ children }: CurrentAccountLayoutProps) => {
+  const { t } = useTranslation()
+
   useQueryCurrentAccount()
-  return <>{children}</>
+
+  const currentAccount = useCurrentAccount()
+
+  if (currentAccount) return <>{children}</>
+  else
+    return (
+      <div className="currentAccount-loader">
+        <LoadingSpinner className="loading-indicator" />
+        <p className="text">{t('CurrentAccount.Layout.loading')}</p>
+      </div>
+    )
 }
 
 export default CurrentAccountLayout
