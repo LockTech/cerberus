5675d70d36dc3cac1f6d3c0b3e8f99a5a7002fd3
Ryan <jesselockard99@gmail.com>
2021-08-02 19:44:13 -0400

    updated AppSettingsModal locales, added hint to theme form field

--- web/src/components/AppSettingsModal/AppSettingsModal.tsx
+++ web/src/components/AppSettingsModal/AppSettingsModal.tsx
@@ -13,7 +13,7 @@ import Select from 'src/components/Select'
 
 import './AppSettingsModal.css'
 
-const ThemeKey = 'App.SettingsModal.form.theme.options'
+const ThemeKey = 'App.Settings.Modal.form.theme.options'
 const ThemeValues = [
   { name: `${ThemeKey}.browser`, value: 'browser' },
   { name: `${ThemeKey}.light`, value: 'light' },
@@ -34,24 +34,25 @@ const AppSettingsModal = () => {
     (data: AppSettingsFormData) => {
       setColorMode(data.theme.value)
       setModalOpen(false)
-      toast.success(t('App.SettingsModal.saved'))
+      toast.success(t('App.Settings.Modal.saved'))
     },
     [setColorMode, setModalOpen, t]
   )
 
   return (
     <Modal open={modalOpen} onClose={() => setModalOpen(false)}>
-      <div className="card card-body space-y-6">
+      <div className="card card-body">
         <div className="title-group">
-          <Modal.Title>{t('App.SettingsModal.title')}</Modal.Title>
+          <Modal.Title>{t('App.Settings.Modal.title')}</Modal.Title>
           <Modal.Description>
-            {t('App.SettingsModal.subtitle')}
+            {t('App.Settings.Modal.subtitle')}
           </Modal.Description>
         </div>
         <Form className="form" onSubmit={onSubmit}>
+          {/* Theme */}
           <div className="input-group">
-            <Label name="theme" className="input-label">
-              {t('App.SettingsModal.form.theme.label')}
+            <Label className="input-label" name="theme">
+              {t('App.Settings.Modal.form.theme.label')}
             </Label>
             <Select
               defaultValue={{
@@ -61,9 +62,12 @@ const AppSettingsModal = () => {
               name="theme"
               values={ThemeValues}
             />
+            <Label className="hint" name="theme">
+              {t('App.Settings.Modal.form.theme.hint')}
+            </Label>
           </div>
           <Submit className="button-primary-fill form-button">
-            {t('App.SettingsModal.form.submit')}
+            {t('App.Settings.Modal.form.submit')}
           </Submit>
         </Form>
       </div>
