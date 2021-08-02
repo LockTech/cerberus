cb0a2f72999e1939b07cedda3ebff421f5c2d2fc
Ryan <jesselockard99@gmail.com>
2021-08-02 15:58:16 -0400

    impl structure for OrganizationUpdateCell's Success component

--- web/src/components/OrganizationUpdateCell/OrganizationUpdateCell.tsx
+++ web/src/components/OrganizationUpdateCell/OrganizationUpdateCell.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next'
+import { FieldError, Form, Label, Submit, TextField } from '@redwoodjs/forms'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
 import type { OrganizationUpdateQuery } from 'types/graphql'
@@ -21,5 +23,43 @@ export const Failure = ({ error }: CellFailureProps) => (
 export const Success = ({
   organization,
 }: CellSuccessProps<OrganizationUpdateQuery>) => {
-  return <div className="card card-body">{JSON.stringify(organization)}</div>
+  const { t } = useTranslation()
+
+  return (
+    <div className="card card-body space-y-6">
+      <div className="title-group">
+        <h2 className="title">{t('Organization.Update.Cell.Success.title')}</h2>
+        <p className="hint">{t('Organization.Update.Cell.Success.subtitle')}</p>
+      </div>
+      <Form className="form">
+        <div className="input-group">
+          <Label
+            className="input-label"
+            errorClassName="input-label-error"
+            name="name"
+          >
+            {t('Organization.Update.Cell.Success.form.name.label')}
+          </Label>
+          <TextField
+            className="input-primary"
+            defaultValue={organization.name}
+            errorClassName="input-error"
+            name="name"
+            validation={{
+              required: {
+                value: true,
+                message: t(
+                  'Organization.Update.Cell.Success.form.name.required'
+                ),
+              },
+            }}
+          />
+          <FieldError className="input-field-error" name="name" />
+        </div>
+        <Submit className="button-primary-fill form-button">
+          {t('Organization.Update.Cell.Success.form.submit')}
+        </Submit>
+      </Form>
+    </div>
+  )
 }
