b3b728dcd841a151f9e5e0e408a8b32caaf2373f
Ryan <jesselockard99@gmail.com>
2021-08-03 02:47:24 -0400

    added UpdateMutation to OrganiationUpdateCell

--- web/src/components/OrganizationUpdateCell/OrganizationUpdateCell.tsx
+++ web/src/components/OrganizationUpdateCell/OrganizationUpdateCell.tsx
@@ -1,8 +1,22 @@
+import { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
 import { useTranslation } from 'react-i18next'
+import { useMutation } from '@apollo/client'
 import { FieldError, Form, Label, Submit, TextField } from '@redwoodjs/forms'
-import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+import { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/toast'
+
+import { QUERY as CurrentAccountQuery } from 'src/hooks/useCurrentAccount'
 
 import type { OrganizationUpdateQuery } from 'types/graphql'
+
+export const MUTATION = gql`
+  mutation OrganizationUpdateMutation($name: String!) {
+    organization: updateOrganization(name: $name) {
+      __typename
+    }
+  }
+`
 
 export const QUERY = gql`
   query OrganizationUpdateQuery {
@@ -20,18 +34,50 @@ export const Failure = ({ error }: CellFailureProps) => (
   <div style={{ color: 'red' }}>Error: {error.message}</div>
 )
 
+interface OrganizationUpdateFormData {
+  name: string
+}
 export const Success = ({
   organization,
 }: CellSuccessProps<OrganizationUpdateQuery>) => {
+  const formMethods = useForm<OrganizationUpdateFormData>({
+    defaultValues: organization,
+    mode: 'all',
+  })
+  const { isDirty } = formMethods.formState
+
   const { t } = useTranslation()
+
+  const onCompleted = useCallback(() => {
+    toast.dismiss()
+    toast.success(t('Organization.Update.Cell.Success.success'))
+  }, [t])
+  const onError = useCallback(() => {}, [])
+
+  const [mutate, { called, loading }] = useMutation(MUTATION, {
+    onCompleted,
+    onError,
+    refetchQueries: [{ query: QUERY }, { query: CurrentAccountQuery }],
+  })
+
+  const onSubmit = useCallback(
+    (variables: OrganizationUpdateFormData) => {
+      if (called) return
+
+      toast.loading(t('Organization.Update.Cell.Success.loading'))
+
+      mutate({ variables })
+    },
+    [called, mutate, t]
+  )
 
   return (
     <div className="card card-body">
       <div className="title-group">
-        <h3 className="title">{t('Organization.Update.Cell.Success.title')}</h3>
+        <h2 className="title">{t('Organization.Update.Cell.Success.title')}</h2>
         <p className="hint">{t('Organization.Update.Cell.Success.subtitle')}</p>
       </div>
-      <Form className="form">
+      <Form className="form" formMethods={formMethods} onSubmit={onSubmit}>
         <div className="input-group">
           <Label
             className="input-label"
@@ -56,7 +102,10 @@ export const Success = ({
           />
           <FieldError className="input-field-error" name="name" />
         </div>
-        <Submit className="button-primary-fill form-button">
+        <Submit
+          className="button-primary-fill form-button"
+          disabled={!isDirty || loading}
+        >
           {t('Organization.Update.Cell.Success.form.submit')}
         </Submit>
       </Form>
