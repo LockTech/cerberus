fedbad4194e432dc24e34e063960896689c70059
Ryan <jesselockard99@gmail.com>
2021-08-05 04:47:00 -0400

    added Helmet and AccountInviteModal to ListAccountsPage

--- web/src/pages/ListAccountsPage/ListAccountsPage.tsx
+++ web/src/pages/ListAccountsPage/ListAccountsPage.tsx
@@ -1,13 +1,19 @@
 import { useTranslation } from 'react-i18next'
+import { Helmet } from '@redwoodjs/web'
 
 import AppbarTitle from 'src/components/Appbar/AppbarTitle'
+import AccountInviteModal from 'src/components/AccountInviteModal/AccountInviteModal'
 
 const ListAccountsPage = () => {
   const { t } = useTranslation()
 
   return (
     <>
+      <Helmet>
+        <title>{t('Accounts.List.Page.Helmet.title')}</title>
+      </Helmet>
       <AppbarTitle>{t('Accounts.List.Page.Appbar.title')}</AppbarTitle>
+      <AccountInviteModal />
     </>
   )
 }
