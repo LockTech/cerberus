bbbae472497fa9af321aea51901c3487d96e0dff
Ryan <jesselockard99@gmail.com>
2021-08-06 02:04:48 -0400

    removed Empty component from AccountListCell

--- web/src/components/AccountListCell/AccountListCell.stories.tsx
+++ web/src/components/AccountListCell/AccountListCell.stories.tsx
@@ -1,14 +1,16 @@
 import type { Meta } from '@storybook/react'
 
-import { Empty, Failure, Loading, Success } from './AccountListCell'
+import { Failure, Loading, Success } from './AccountListCell'
 import { standard } from './AccountListCell.mock'
 
 export default {
-  subcomponents: { Empty, Failure, Loading, Success },
+  subcomponents: { Failure, Loading, Success },
   title: 'Cells/AccountListCell',
 } as Meta
 
-export const failure = () => <Failure error={new Error('Oh no!')} />
+export const databaseFailure = () => (
+  <Failure error={new Error('account-get')} />
+)
 
 export const loading = () => <Loading />
 
--- web/src/components/AccountListCell/AccountListCell.tsx
+++ web/src/components/AccountListCell/AccountListCell.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next'
 import type { CellSuccessProps, CellFailureProps } from '@redwoodjs/web'
 
+import FailureCard from 'src/components/FailureCard'
 import LoadingCard from 'src/components/LoadingCard'
 
 import type { AccountListQuery } from 'types/graphql'
@@ -27,11 +28,18 @@ export const Loading = () => {
   )
 }
 
-export const Empty = () => <div>Empty</div>
+export const Failure = ({ error }: CellFailureProps) => {
+  const { t } = useTranslation()
 
-export const Failure = ({ error }: CellFailureProps) => (
-  <div style={{ color: 'red' }}>Error: {error.message}</div>
-)
+  return (
+    <FailureCard>
+      <p className="text">{t('Account.List.Cell.failure.title')}</p>
+      <p className="text">
+        {t(`Account.List.Cell.failure.errors.${error.message}`, error.message)}
+      </p>
+    </FailureCard>
+  )
+}
 
 export const Success = ({ accounts }: CellSuccessProps<AccountListQuery>) => {
   return (
