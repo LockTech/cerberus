513090036c7444b2145cc2aa69d4b5e2a91b1078
Ryan <jesselockard99@gmail.com>
2021-08-06 03:40:49 -0400

    impl AccountPage

--- web/src/pages/AccountPage/AccountPage.tsx
+++ web/src/pages/AccountPage/AccountPage.tsx
@@ -1,16 +1,25 @@
-import { Link, routes } from '@redwoodjs/router'
+import { useTranslation } from 'react-i18next'
+import { Helmet } from '@redwoodjs/web'
 
-const AccountPage = () => {
+import AppbarTitle from 'src/components/Appbar/AppbarTitle'
+import AccountUpdateCell from 'src/components/AccountUpdateCell'
+
+export interface AccountPageProps {
+  id: string
+}
+
+const AccountPage = ({ id }: AccountPageProps) => {
+  const { t } = useTranslation()
+
   return (
     <>
-      <h1>AccountPage</h1>
-      <p>
-        Find me in <code>./web/src/pages/AccountPage/AccountPage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>account</code>, link to me with `
-        <Link to={routes.account()}>Account</Link>`
-      </p>
+      <Helmet>
+        <title>{t('Account.Page.Helmet.title')}</title>
+      </Helmet>
+      <AppbarTitle>{t('Account.Page.Appbar.title')}</AppbarTitle>
+      <div className="page-layout">
+        <AccountUpdateCell id={id} />
+      </div>
     </>
   )
 }
