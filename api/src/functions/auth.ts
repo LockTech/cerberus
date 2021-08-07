6d7a0a31736a3be922293ef6899a1f616dabe2ea
Ryan <jesselockard99@gmail.com>
2021-08-06 21:10:15 -0400

    updated configuration of DbHandler for auth function

--- api/src/functions/auth.ts
+++ api/src/functions/auth.ts
@@ -1,35 +1,46 @@
+import type { APIGatewayEvent } from 'aws-lambda'
 import { DbAuthHandler } from '@redwoodjs/api'
+import type { GlobalContext } from '@redwoodjs/api'
 
 import { signupHandler } from 'src/helpers/signup'
 
 import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
 
+// ==
+const authModelAccessor = 'account'
+const authFields = {
+  id: 'id',
+  username: 'email',
+  hashedPassword: 'hashedPassword',
+  salt: 'salt',
+}
 const loginExpires = 60 * 60 * 24 // * 365 * 10,
+//
 
-export const handler = async (event, context) => {
-  logger.trace('Invoking auth handler.')
-
-  const authHandler = new DbAuthHandler(event, context, {
+// ==
+const initAuthHandler = (event: APIGatewayEvent, context: GlobalContext) =>
+  new DbAuthHandler(event, context, {
     db,
-
-    authModelAccessor: 'account',
-
-    authFields: {
-      id: 'id',
-      username: 'email',
-      hashedPassword: 'hashedPassword',
-      salt: 'salt',
-    },
-
+    authModelAccessor,
+    authFields,
     signupHandler,
-
     loginExpires,
   })
+//
 
+// ==
+export const handler = async (
+  event: APIGatewayEvent,
+  context: GlobalContext
+) => {
+  logger.trace('Invoking auth handler.')
+
+  const authHandler = initAuthHandler(event, context)
   const res = await authHandler.invoke()
 
   logger.info('Successfully invoked auth handler')
 
   return res
 }
+//
