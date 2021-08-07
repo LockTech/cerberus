1e96930ffb3d4cbccbc1f344d081304c92b89e63
Ryan <jesselockard99@gmail.com>
2021-08-07 19:30:42 -0400

    updated email test to use new validator API

--- api/src/validators/email/email.test.ts
+++ api/src/validators/email/email.test.ts
@@ -24,35 +24,35 @@ const CharacterError: Error = {
 
 describe('email validator', () => {
   it('throws when email is not provided', () => {
-    expect(() =>
-      validateEmail(ServiceName, { input: { email: undefined } })
-    ).toThrow(RequiredError)
-    expect(() =>
-      validateEmail(ServiceName, { input: { email: null } })
-    ).toThrow(RequiredError)
+    expect(() => validateEmail(ServiceName, { email: undefined })).toThrow(
+      RequiredError
+    )
+    expect(() => validateEmail(ServiceName, { email: null })).toThrow(
+      RequiredError
+    )
   })
 
   it('throws when email is not a string', () => {
     /// @ts-expect-error checking failing functionality
-    expect(() => validateEmail(ServiceName, { input: { email: 42 } })).toThrow(
+    expect(() => validateEmail(ServiceName, { email: 42 })).toThrow(
       InvalidError
     )
     expect(() =>
       /// @ts-expect-error checking failing functionality
-      validateEmail(ServiceName, { input: { email: false } })
+      validateEmail(ServiceName, { email: false })
     ).toThrow(InvalidError)
     expect(() =>
       /// @ts-expect-error checking failing functionality
-      validateEmail(ServiceName, { input: { email: () => 421 } })
+      validateEmail(ServiceName, { email: () => 421 })
     ).toThrow(InvalidError)
     expect(() =>
       /// @ts-expect-error checking failing functionality
-      validateEmail(ServiceName, { input: { email: 423.22 } })
+      validateEmail(ServiceName, { email: 423.22 })
     ).toThrow(InvalidError)
   })
 
   it('throws when email is an empty string', () => {
-    expect(() => validateEmail(ServiceName, { input: { email: '' } })).toThrow(
+    expect(() => validateEmail(ServiceName, { email: '' })).toThrow(
       InvalidError
     )
   })
@@ -60,48 +60,46 @@ describe('email validator', () => {
   it('throws when email is longer than 254 characters', () => {
     expect(() =>
       validateEmail(ServiceName, {
-        input: {
-          email:
-            '444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444',
-        },
+        email:
+          '444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444',
       })
     ).toThrow(LengthError)
   })
 
   it('throws when email contains reserved characters', () => {
     expect(() =>
-      validateEmail(ServiceName, { input: { email: 'foobar$@example.com' } })
+      validateEmail(ServiceName, { email: 'foobar$@example.com' })
     ).toThrow(CharacterError)
     //
     expect(() =>
       validateEmail(ServiceName, {
-        input: { email: 'fodad#fa2$17sdf(fa|fa!d@example.com' },
+        email: 'fodad#fa2$17sdf(fa|fa!d@example.com',
       })
     ).toThrow(CharacterError)
     //
     expect(() =>
-      validateEmail(ServiceName, { input: { email: "fodad'obar@example.com" } })
+      validateEmail(ServiceName, { email: "fodad'obar@example.com" })
     ).toThrow(CharacterError)
     //
     expect(() =>
-      validateEmail(ServiceName, { input: { email: 'fodad"obar@example.com' } })
-    ).toThrow(CharacterError)
-    //
-    expect(() =>
-      validateEmail(ServiceName, {
-        input: { email: 'fodadconsole.log("obar");@example.com' },
-      })
+      validateEmail(ServiceName, { email: 'fodad"obar@example.com' })
     ).toThrow(CharacterError)
     //
     expect(() =>
       validateEmail(ServiceName, {
-        input: { email: 'foda_adwk@ex_ample.com' },
+        email: 'fodadconsole.log("obar");@example.com',
       })
     ).toThrow(CharacterError)
     //
     expect(() =>
       validateEmail(ServiceName, {
-        input: { email: 'fo..da_adwk@example.com' },
+        email: 'foda_adwk@ex_ample.com',
+      })
+    ).toThrow(CharacterError)
+    //
+    expect(() =>
+      validateEmail(ServiceName, {
+        email: 'fo..da_adwk@example.com',
       })
     ).toThrow(CharacterError)
   })
