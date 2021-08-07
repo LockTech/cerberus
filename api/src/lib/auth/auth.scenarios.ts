2b56881d531669a427922ebb6d42a0613769f410
Ryan <jesselockard99@gmail.com>
2021-08-07 19:53:02 -0400

    updated auth lib scenario

--- api/src/lib/auth/auth.scenarios.ts
+++ api/src/lib/auth/auth.scenarios.ts
@@ -10,7 +10,9 @@ export const standard = defineScenario({
       email: 'foo.bar@acme.corp',
       name: 'Foo Bar',
       id: '00fa2b1a-40d1-4c32-be0c-516d86872970',
-      organizationId: '0251d504-347b-4880-9783-db6d199e02ee',
+      organization: {
+        connect: { id: '0251d504-347b-4880-9783-db6d199e02ee' },
+      },
       verified: false,
     },
   },
