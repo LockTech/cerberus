abf7e0afc1c74c40eeb83ae585810764e0bbf1aa
Ryan <jesselockard99@gmail.com>
2021-08-08 04:01:01 -0400

    updated documentation for account validators, added note about db assertions

--- api/src/validators/account/account.ts
+++ api/src/validators/account/account.ts
@@ -15,9 +15,10 @@ interface EmailInput {
 }
 /**
  * @throws
- *  * 'email-invalid' - When `email` is not a string.
- *  * 'email-length' - When `email` is less than 1 or greater than `AccountEmailMaxLength` characters long.
- *  * 'email-reserved' - When `email` contains a reserved character.
+ *  * 'account-email-invalid' - When `email` is not a string.
+ *  * 'account-email-length' - When `email` is less than 1 or greater than `AccountEmailMaxLength` characters long.
+ *  * 'account-email-reserved' - When `email` contains a reserved character.
+ *  * 'account-email-taken' - When `email` is in use by another account.
  */
 export const validateAccountEmail = (
   service: string,
@@ -37,6 +38,8 @@ export const validateAccountEmail = (
     logger.error({ service }, 'Error validating account email.')
     throw new ValidationError('account-email-reserved')
   }
+
+  // perform DB check for email's existing use
 }
 
 interface IDInput {
@@ -44,7 +47,8 @@ interface IDInput {
 }
 /**
  * @throws
- *  * 'account-id-invalid' - When `name` is not a string.
+ *  * 'account-id-invalid' - When `id` is not a valid UUID.
+ *  * 'account-id-match' - When `id` does not belong to the same organization as `context.currentUser.id`
  */
 export const validateAccountID = (service: string, { id }: IDInput) => {
   if (!isStr(id) || !validateUUID(id)) {
