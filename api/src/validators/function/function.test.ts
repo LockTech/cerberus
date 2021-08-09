c87c95bfe11894b512197766d3ea1b510d08d796
Ryan <jesselockard99@gmail.com>
2021-08-09 17:23:24 -0400

    updated function validator's tests

--- api/src/validators/function/function.test.ts
+++ api/src/validators/function/function.test.ts
@@ -20,52 +20,73 @@ const MethodInvalid = {
 
 const Service = 'foo'
 
+const body = ''
+const headers = { 'content-type': 'application/json' }
+
+/* eslint-disable prettier/prettier */
 describe('function validator', () => {
-  /* eslint-disable prettier/prettier */
   describe('validateJSONBody', () => {
     it('throws when `body` is defined and not a string', () => {
       // @ts-expect-error checking failing functionality
-      expect(() => validateJSONBody(Service, { body: 1 })).toThrow(BodyInvalid)
+      expect(() => validateJSONBody(Service, { headers, body: 1 })).toThrow(BodyInvalid)
       // @ts-expect-error checking failing functionality
-      expect(() => validateJSONBody(Service, { body: true })).toThrow(BodyInvalid)
+      expect(() => validateJSONBody(Service, { headers, body: true })).toThrow(BodyInvalid)
       // @ts-expect-error checking failing functionality
-      expect(() => validateJSONBody(Service, { body: false })).toThrow(BodyInvalid)
+      expect(() => validateJSONBody(Service, { headers, body: false })).toThrow(BodyInvalid)
       // @ts-expect-error checking failing functionality
-      expect(() => validateJSONBody(Service, { body: () => 1 })).toThrow(BodyInvalid)
+      expect(() => validateJSONBody(Service, { headers, body: () => 1 })).toThrow(BodyInvalid)
+    })
+
+    it('DOES NOT throw when `body` is undefined', () => {
+      // @ts-expect-error checking failing functionality
+      expect(() => validateJSONBody(Service, { headers, body: undefined })).not.toThrow(BodyInvalid)
+      // @ts-expect-error checking failing functionality
+      expect(() => validateJSONBody(Service, { headers, body: null })).not.toThrow(BodyInvalid)
     })
 
     it(`throws when \`headers\` does not have ${FunctionHeaderMediaTypeKey} defined`, () => {
       expect(
         // @ts-expect-error checking failing functionality
-        () => validateJSONBody(Service, { body: '', headers: { 'content-type': undefined } })
+        () => validateJSONBody(Service, { body, headers: { 'content-type': undefined } })
       ).toThrow(MediaTypeInvalid)
       expect(
         // @ts-expect-error checking failing functionality
-        () => validateJSONBody(Service, { body: '', headers: { 'content-type': null } })
+        () => validateJSONBody(Service, { body, headers: { 'content-type': null } })
       ).toThrow(MediaTypeInvalid)
     })
 
     it(`throws when \`headers\` does not have ${FunctionHeaderMediaTypeKey} set to ${FunctionHeaderMediaType}`, () => {
       expect(
          // @ts-expect-error checking failing functionality
-        () => validateJSONBody(Service, { body: '', headers: { 'content-type': false } })
+        () => validateJSONBody(Service, { body, headers: { 'content-type': false } })
       ).toThrow(MediaTypeInvalid)
+      //
       expect(
         // @ts-expect-error checking failing functionality
-        () => validateJSONBody(Service, { body: '', headers: { 'content-type': 4 } })
+        () => validateJSONBody(Service, { body, headers: { 'content-type': 4 } })
       ).toThrow(MediaTypeInvalid)
+      //
       expect(
         // @ts-expect-error checking failing functionality
-        () => validateJSONBody(Service, { body: '', headers: { 'content-type': () => 4 } })
+        () => validateJSONBody(Service, { body, headers: { 'content-type': () => 4 } })
       ).toThrow(MediaTypeInvalid)
+      //
         expect(
         // @ts-expect-error checking failing functionality
-        () => validateJSONBody(Service, { body: '', headers: { 'content-type': '' } })
+        () => validateJSONBody(Service, { body, headers: { 'content-type': '' } })
       ).toThrow(MediaTypeInvalid)
+      //
       expect(
         // @ts-expect-error checking failing functionality
-        () => validateJSONBody(Service, { body: '', headers: { 'content-type': 'foo' } })
+        () => validateJSONBody(Service, { body, headers: { 'content-type': 'foo' } })
       ).toThrow(MediaTypeInvalid)
+    })
+
+    it(`DOES NOT throw when \`headers\` have ${FunctionHeaderMediaTypeKey} set to ${FunctionHeaderMediaType}`, () => {
+      expect(
+        // @ts-expect-error checking failing functionality
+        () => validateJSONBody(Service, { body, headers })
+      )
     })
   })
 
@@ -101,5 +122,5 @@ describe('function validator', () => {
       expect(() => validateHTTPMethod(Service, { httpMethod: 'PUT' })).not.toThrow(MethodInvalid)
     })
   })
-  /* eslint-enable prettier/prettier */
 })
+/* eslint-enable prettier/prettier */
