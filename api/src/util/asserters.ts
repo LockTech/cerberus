1d803a8b4a7fb6f2f148f2c9423fa399c36eb1e1
Ryan <jesselockard99@gmail.com>
2021-08-08 23:57:44 -0400

    updated types for all asserters

--- api/src/util/asserters.ts
+++ api/src/util/asserters.ts
@@ -1,14 +1,15 @@
-export const isFunc = (value: unknown) => {
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const isFunc = (value: unknown): value is Function => {
   if (typeof value === 'function') return true
   else return false
 }
 
-export const isStr = (value: unknown) => {
+export const isStr = (value: unknown): value is string => {
   if (typeof value === 'string') return true
   else return false
 }
 
-export const isUndefined = (value: unknown) => {
+export const isUndefined = (value: unknown): value is undefined => {
   if (value === undefined || value === null) return true
   else return false
 }
