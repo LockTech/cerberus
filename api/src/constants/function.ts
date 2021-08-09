7251c413f1110176d413fe3fa47241773e88cf96
Ryan <jesselockard99@gmail.com>
2021-08-09 17:01:11 -0400

    created function constants

---
+++ api/src/constants/function.ts
@@ -0,0 +1,6 @@
+export const FunctionHeaderMediaTypeKey = 'content-type'
+export const FunctionHeaderMediaType = 'application/json'
+
+export const FunctionDefaultHeaders = {
+  [FunctionHeaderMediaTypeKey]: FunctionHeaderMediaType,
+}
