5ea0dfce1c511cc92432142b1f5b90f1c07a6664
Ryan <jesselockard99@gmail.com>
2021-08-08 20:49:23 -0400

    updated isDefined and isUndefined asserters

--- api/src/util/asserters.ts
+++ api/src/util/asserters.ts
@@ -1,10 +1,18 @@
+export const isFunc = (value: unknown) => {
+  if (typeof value === 'function') return true
+  else return false
+}
+
 export const isStr = (value: unknown) => {
   if (typeof value === 'string') return true
   else return false
 }
 
 export const isUndefined = (value: unknown) => {
-  if (typeof value === 'undefined') return true
+  if (value === undefined || value === null) return true
   else return false
 }
-export const isDefined = (value: unknown) => !isUndefined(value)
+export const isDefined = (value: unknown) => {
+  if (value !== undefined && value !== null) return true
+  else return false
+}
