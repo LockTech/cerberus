b86f49b7818a129bff9d89bdd605e5068374d277
Ryan <jesselockard99@gmail.com>
2021-08-09 00:00:22 -0400

    added organization to getCurrentUser query

--- api/src/lib/auth/auth.ts
+++ api/src/lib/auth/auth.ts
@@ -1,11 +1,16 @@
+import type { Account, Organization } from '@prisma/client'
+
 import { db } from 'src/lib/db'
 
 import { removeAuthFields } from 'src/services/accounts'
 
 export const getCurrentUser = async (session) => {
-  let res = await db.account.findUnique({ where: { id: session.id } })
+  let res = await db.account.findUnique({
+    include: { organization: true },
+    where: { id: session.id },
+  })
 
-  res = removeAuthFields(res)
+  res = removeAuthFields(res) as Account & { organization: Organization }
 
   return res
 }
