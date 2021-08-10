9bf43182708635ea52a63dc2d494789b4d12cfe6
Ryan <jesselockard99@gmail.com>
2021-08-10 19:01:27 -0400

    created keto constants

---
+++ api/src/constants/keto.ts
@@ -0,0 +1,4 @@
+export const KetoURL = process.env.KETO_URL
+export const CheckURL = `${KetoURL}/check`
+export const DeleteURL = `${KetoURL}/relation-tuples`
+export const WriteURL = `${KetoURL}/relation-tuples`
