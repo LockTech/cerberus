dc5a6a9248204357fcad03c17b9447c2db9f6709
Ryan <jesselockard99@gmail.com>
2021-08-12 17:27:39 -0400

    created isBool asserter

--- api/src/util/asserters/asserters.ts
+++ api/src/util/asserters/asserters.ts
@@ -1,23 +1,28 @@
 import type { PermissionTuple } from 'src/constants/permission'
 
+export const isBool = (value: unknown): value is boolean => {
+  if (typeof value === 'boolean') return true
+  return false
+}
+
 // eslint-disable-next-line @typescript-eslint/ban-types
 export const isFunc = (value: unknown): value is Function => {
   if (typeof value === 'function') return true
-  else return false
+  return false
 }
 
 export const isStr = (value: unknown): value is string => {
   if (typeof value === 'string') return true
-  else return false
+  return false
 }
 
 export const isUndefined = (value: unknown): value is undefined => {
   if (value === undefined || value === null) return true
-  else return false
+  return false
 }
 export const isDefined = (value: unknown) => {
   if (value !== undefined && value !== null) return true
-  else return false
+  return false
 }
 
 export const isPermissionTuple = ({
