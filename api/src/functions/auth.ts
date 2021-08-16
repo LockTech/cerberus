d3a0cd3b20ca4660712c34f42947802564d1f3e2
Ryan <jesselockard99@gmail.com>
2021-08-16 02:41:44 -0400

    removed initAuthHandler

--- api/src/functions/auth.ts
+++ api/src/functions/auth.ts
@@ -9,22 +9,19 @@ import { signupHandler } from 'src/helpers/signup'
 import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
 
-const initAuthHandler = (event: APIGatewayEvent, context: GlobalContext) =>
-  new DbAuthHandler(event, context, {
-    db,
-    authModelAccessor,
-    authFields,
-    signupHandler,
-    loginExpires,
-  })
-
 export const handler = async (
   event: APIGatewayEvent,
   context: GlobalContext
 ) => {
   logger.trace('Invoking auth handler.')
 
-  const authHandler = initAuthHandler(event, context)
+  const authHandler = new DbAuthHandler(event, context, {
+    db,
+    authModelAccessor,
+    authFields,
+    signupHandler,
+    loginExpires,
+  })
   const res = await authHandler.invoke()
 
   logger.info('Successfully invoked auth handler')
