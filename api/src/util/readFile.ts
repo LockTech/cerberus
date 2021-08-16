457a9f49595a50a9bc28678e91fdb63fb3e3948d
Ryan <jesselockard99@gmail.com>
2021-08-16 02:42:58 -0400

    removed try-catch from readFile utility

--- api/src/util/readFile.ts
+++ api/src/util/readFile.ts
@@ -5,20 +5,11 @@ import { isStr } from 'src/util/asserters'
 /**
  * @throws
  *  * 'file-path' - When `path` is not a string
- *  * 'file-read' - When reading the file at `path` and converting it to a `string` fails.
  */
 export const readFile = (path: string) => {
   if (!isStr(path)) {
     throw new Error('file-path')
   }
 
-  let res: string
-
-  try {
-    res = readFileSync(path).toString()
-  } catch (err) {
-    throw new Error('file-read')
-  }
-
-  return res
+  return readFileSync(path).toString()
 }
