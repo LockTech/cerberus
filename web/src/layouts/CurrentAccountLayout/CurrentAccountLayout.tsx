b418598d2d374769aa6e0780015f4cb1b23a31b3
Ryan <jesselockard99@gmail.com>
2021-08-16 06:37:23 -0400

    added use of Redwood's useAuth loading to CurrentAccountLayout

--- web/src/layouts/CurrentAccountLayout/CurrentAccountLayout.tsx
+++ web/src/layouts/CurrentAccountLayout/CurrentAccountLayout.tsx
@@ -1,6 +1,7 @@
 import { useTranslation } from 'react-i18next'
+import { useAuth } from '@redwoodjs/auth'
 
-import LoadingSpinner from 'src/components/LoadingSpinner/LoadingSpinner'
+import LoadingSpinner from 'src/components/LoadingSpinner'
 
 import { useCurrentAccount } from 'src/hooks/useCurrentAccount'
 
@@ -13,9 +14,11 @@ type CurrentAccountLayoutProps = {
 const CurrentAccountLayout = ({ children }: CurrentAccountLayoutProps) => {
   const { t } = useTranslation()
 
+  const { loading } = useAuth()
+
   const currentAccount = useCurrentAccount()
 
-  if (currentAccount) return <>{children}</>
+  if (currentAccount && !loading) return <>{children}</>
   else
     return (
       <div className="currentAccount-loader">
