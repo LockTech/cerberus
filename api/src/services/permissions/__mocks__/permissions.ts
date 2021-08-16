9678821cb629391c08b73acdc2b10989af7c5547
Ryan <jesselockard99@gmail.com>
2021-08-16 02:42:20 -0400

    removed mocked services

--- api/src/services/permissions/__mocks__/permissions.ts
+++
@@ -1,9 +0,0 @@
-export const createPermission = jest.fn()
-export const permission = jest.fn()
-export const permissions = jest.fn()
-
-beforeEach(() => {
-  createPermission.mockClear()
-  permission.mockClear()
-  permissions.mockClear()
-})
