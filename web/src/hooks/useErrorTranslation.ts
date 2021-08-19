23b4fd4ae35451a33cfd17254636b293e47c113b
Ryan <jesselockard99@gmail.com>
2021-08-19 06:22:30 -0400

    updated documentation for useErrorTranslation

--- web/src/hooks/useErrorTranslation.ts
+++ web/src/hooks/useErrorTranslation.ts
@@ -6,13 +6,16 @@ import { useTranslation } from 'react-i18next'
  *
  * Provides translations for an API error, falling back to the error's default message.
  *
- * **Unlike** `useTranslation`, the error thrown or caught should be passed directly to the `t` function.
+ * Unlike `useTranslation`, the error thrown or caught should be passed directly to the `t` function.
+ *
+ * **Note:** The `t` function has been renamed to `et` to avoid name conflicts when making use of both
+ * hooks.
  *
  * @example
  * const apiError = new Error('account-read')
  *
  * const component = () => {
- *  const { t } = useErrorTranslation()
+ *  const { et } = useErrorTranslation()
  *
  *  return <p>{t(apiError)}</p>
  * }
@@ -20,10 +23,10 @@ import { useTranslation } from 'react-i18next'
 export const useErrorTranslation = () => {
   const { t: _t, ...rest } = useTranslation()
 
-  const t = useCallback(
+  const et = useCallback(
     ({ message }: Error) => _t(`errors:${message}`, message),
     [_t]
   )
 
-  return { t, ...rest }
+  return { et, ...rest }
 }
