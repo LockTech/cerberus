64755c34f9ed8e565c21401e940c83340b134376
Ryan <jesselockard99@gmail.com>
2021-08-19 20:22:35 -0400

    updated Tailwind

--- web/src/config/tailwind.config.js
+++ web/src/config/tailwind.config.js
@@ -2,9 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: [
-    './public/**/*.{js,ts,jsx,tsx,html,css}',
-    './src/**/*.{js,ts,jsx,tsx,html,css}',
-    './config/**/*.{js,ts,jsx,tsx,html,css}',
+    '../**/*.{js,ts,jsx,tsx,html,css}',
   ],
   darkMode: 'class', // or 'media' or 'class'
   mode: 'jit',
