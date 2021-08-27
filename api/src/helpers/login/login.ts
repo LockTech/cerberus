f10d001ea3d7c37d5a7059894eaccb2fc67c1293
Ryan <jesselockard99@gmail.com>
2021-08-27 18:12:34 -0400

    added verified and disabled validators to loginHandler

--- api/src/helpers/login/login.ts
+++ api/src/helpers/login/login.ts
@@ -1,3 +1,15 @@
 import type { Account } from '@prisma/client'
+import { setContext } from '@redwoodjs/api'
 
-export const loginHandler = async (user: Account) => user
+import { validateAuthDisabled, validateAuthVerified } from 'src/validators/auth'
+
+export const loginHandler = async (currentUser: Account) => {
+  setContext({ currentUser })
+
+  validateAuthVerified('loginHandler')
+  validateAuthDisabled('loginHandler')
+
+  // MFA?
+
+  return currentUser
+}
