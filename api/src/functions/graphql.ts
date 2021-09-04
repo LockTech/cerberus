1f91604672f5721ea835520d90e598f12329b60b
Ryan <jesselockard99@gmail.com>
2021-09-03 21:57:53 -0400

    reordered imports on graphql function

--- api/src/functions/graphql.ts
+++ api/src/functions/graphql.ts
@@ -5,12 +5,11 @@ import {
 } from '@redwoodjs/graphql-server'
 
 import schemas from 'src/graphql/**/*.{js,ts}'
+import services from 'src/services/**/*.{js,ts}'
 
 import { getCurrentUser } from 'src/lib/auth'
 import { db } from 'src/lib/db'
 import { logger } from 'src/lib/logger'
-
-import services from 'src/services/**/*.{js,ts}'
 
 export const handler = createGraphQLHandler({
   getCurrentUser,
