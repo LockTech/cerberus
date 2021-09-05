a1477b2ec04f66b58fba591b7b56def4d02e267c
Ryan <jesselockard99@gmail.com>
2021-09-05 00:33:00 -0400

    reimpl AuthLayout

--- web/src/layouts/AuthLayout/AuthLayout.tsx
+++ web/src/layouts/AuthLayout/AuthLayout.tsx
@@ -2,9 +2,7 @@ import { useTranslation } from 'react-i18next'
 
 import BrandBanner from 'src/components/BrandBanner'
 
-import { PrivacyURL } from 'src/constants/variables'
-
-import './AuthLayout.css'
+import { PrivacyURL, SupportURL } from 'src/constants/variables'
 
 type AuthLayoutProps = {
   children: React.ReactNode
@@ -14,12 +12,15 @@ const AuthLayout = ({ children }: AuthLayoutProps) => {
   const { t } = useTranslation()
 
   return (
-    <div className="auth-layout">
+    <div className="max-w-md m-auto pt-8 space-y-6">
       <BrandBanner />
       {children}
-      <div className="auth-link">
-        <a className="link-primary" href={PrivacyURL}>
+      <div className="flex flex-col items-center space-y-3">
+        <a className="link" href={PrivacyURL} rel="noreferrer" target="_blank">
           {t('Auth.Layout.privacy')}
+        </a>
+        <a className="link" href={SupportURL} rel="noreferrer" target="_blank">
+          {t('Auth.Layout.support')}
         </a>
       </div>
     </div>
