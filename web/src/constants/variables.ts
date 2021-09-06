ae23e023079d9e812037877e99c4af392d1c2eb7
Ryan <jesselockard99@gmail.com>
2021-09-06 17:26:58 -0400

    added BRAND_URL constant

--- web/src/constants/variables.ts
+++ web/src/constants/variables.ts
@@ -1,5 +1,7 @@
 export const CopyrightURL = 'https://locktech.software'
 
+export const BrandURL = process.env.BRAND_URL
+
 export const PrivacyURL = process.env.PRIVACY_URL
 
 export const SupportURL = process.env.SUPPORT_URL
