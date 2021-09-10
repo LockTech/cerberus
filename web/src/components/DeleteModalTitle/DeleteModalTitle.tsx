9b2b85af64d134f0d5f8eaa9a71ef72479da2469
Ryan <jesselockard99@gmail.com>
2021-09-10 18:17:53 -0400

    added UndoneNotice and DeleteModalTitle components

---
+++ web/src/components/DeleteModalTitle/DeleteModalTitle.tsx
@@ -0,0 +1,18 @@
+import { TrashIcon } from '@heroicons/react/outline'
+
+export type DeleteModalTitleProps = {
+  children: React.ReactNode
+}
+
+const DeleteModalTitle = ({ children }: DeleteModalTitleProps) => {
+  return (
+    <div className="flex flex-row items-center space-x-4 sm:space-x-6">
+      <div className="text-red-600 dark:text-red-400 h-10 w-10">
+        <TrashIcon />
+      </div>
+      <div className="space-y-1">{children}</div>
+    </div>
+  )
+}
+
+export default DeleteModalTitle
