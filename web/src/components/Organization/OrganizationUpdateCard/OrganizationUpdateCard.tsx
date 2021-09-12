b856e2e88230a4af1fbd24f39826ba452ab35e7c
Ryan <jesselockard99@gmail.com>
2021-09-12 02:49:08 -0400

    created OrganizationUpdateCard

---
+++ web/src/components/Organization/OrganizationUpdateCard/OrganizationUpdateCard.tsx
@@ -0,0 +1,125 @@
+import { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import { useTranslation } from 'react-i18next'
+import { useMutation } from '@apollo/client'
+import { useAuth } from '@redwoodjs/auth'
+import { FieldError, Form, Label, Submit, TextField } from '@redwoodjs/forms'
+import { toast } from '@redwoodjs/web/toast'
+
+import { QUERY } from 'src/components/Organization/OrganizationDetailCell'
+
+import { useErrorTranslation } from 'src/hooks/useErrorTranslation'
+
+import type { OrganizationDetailQuery } from 'types/graphql'
+
+export const MUTATION = gql`
+  mutation OrganizationUpdateMutation($name: String!) {
+    organization: updateOrganization(name: $name) {
+      name
+    }
+  }
+`
+
+export interface OrganizationUpdateFormData {
+  name: string
+}
+
+export type OrganizationUpdateCardProps = {
+  organization: OrganizationDetailQuery['organization']
+}
+
+const OrganizationUpdateCard = ({ organization: { name } }: OrganizationUpdateCardProps) => {
+  const formMethods = useForm<OrganizationUpdateFormData>({
+    defaultValues: { name },
+    mode: 'onSubmit',
+  })
+  const { reset } = formMethods
+  const { isDirty } = formMethods.formState
+
+  const { t } = useTranslation()
+  const { et } = useErrorTranslation()
+
+  const { reauthenticate } = useAuth()
+
+  const onCompleted = useCallback(
+    async ({ name }) => {
+      await reauthenticate()
+      reset({ name })
+
+      toast.dismiss()
+      toast.success(t('Organization.Update.Cell.Success.update.success'))
+    },
+    [reauthenticate, reset, t]
+  )
+  const onError = useCallback(
+    (err: Error) => {
+      toast.dismiss()
+      toast.error(et(err))
+    },
+    [et]
+  )
+  const [mutate, { loading }] = useMutation(
+    MUTATION,
+    {
+      onCompleted,
+      onError,
+      refetchQueries: [{ query: QUERY }],
+    }
+  )
+
+  const onSubmit = useCallback(
+    (variables: OrganizationUpdateFormData) => {
+      if (loading) return
+
+      mutate({ variables })
+    },
+    [loading, mutate]
+  )
+
+  return (
+    <div className="card body">
+      <header className="space-y-1">
+        <h2 className="text title">
+          {t('Organization.Update.Cell.Success.title')}
+        </h2>
+        <p className="muted hint">
+          {t('Organization.Update.Cell.Success.subtitle')}
+        </p>
+      </header>
+      <Form className="form" formMethods={formMethods} onSubmit={onSubmit}>
+        <div className="input-group floating">
+          <TextField
+            autoComplete="organization"
+            className="input"
+            defaultValue={name}
+            errorClassName="input input-error"
+            name="name"
+            placeholder={t(
+              'Organization.Update.Cell.Success.form.name.placeholder'
+            )}
+            validation={{
+              required: {
+                value: true,
+                message: t(
+                  'Organization.Update.Cell.Success.form.name.required'
+                ),
+              },
+            }}
+          />
+          <Label className="input-label" name="name">
+            {t('Organization.Update.Cell.Success.form.name.label')}
+          </Label>
+          <FieldError className="input-error" name="name" />
+        </div>
+        <Submit
+          className="btn btn-primary form-button"
+          disabled={!isDirty || loading}
+        >
+          {t('Organization.Update.Cell.Success.form.submit')}
+        </Submit>
+      </Form>
+    </div>
+  )
+}
+
+export default OrganizationUpdateCard
