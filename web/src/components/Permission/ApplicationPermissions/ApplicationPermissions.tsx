642e553f264a2a63864065fe959c3c13bd52e4d7
Ryan <jesselockard99@gmail.com>
2021-09-15 15:05:40 -0400

    updated markup for ApplicationPermissions, extracted styling to CSS

--- web/src/components/Permission/ApplicationPermissions/ApplicationPermissions.tsx
+++ web/src/components/Permission/ApplicationPermissions/ApplicationPermissions.tsx
@@ -1,21 +1,27 @@
 import clsx from 'clsx'
 import { useTranslation } from 'react-i18next'
 import { Disclosure, Transition } from '@headlessui/react'
-import { ChevronRightIcon } from '@heroicons/react/solid'
+import { ChevronRightIcon } from '@heroicons/react/outline'
 
 import Permission from 'src/components/Permission/Permission'
 import type { PermissionProps } from 'src/components/Permission/Permission'
 
+import type { Permission as TPermission } from 'types/graphql'
+
 import './ApplicationPermissions.css'
 
-export type ApplicationPermissionsProps = {
+export type ApplicationPermissionsProps = Pick<PermissionProps, 'role'> & {
   application: string
-  permissions: PermissionProps[]
+  permissions: Pick<
+    TPermission,
+    'application' | 'id' | 'namespace' | 'object' | 'relation'
+  >[]
 }
 
 const ApplicationPermissions = ({
   application,
   permissions,
+  role,
 }: ApplicationPermissionsProps) => {
   const { t } = useTranslation()
 
@@ -24,7 +30,11 @@ const ApplicationPermissions = ({
       {({ open }) => (
         <>
           <Disclosure.Button className="applicationPermission-button">
-            <div className="flex-grow space-y-1">
+            <ChevronRightIcon
+              aria-hidden="true"
+              className={clsx('icon', open && 'active')}
+            />
+            <div className="space-y-1 w-full">
               <h3 className="font-semibold text text-xl">
                 {t(`permissions:${application}.title`)}
               </h3>
@@ -34,7 +44,6 @@ const ApplicationPermissions = ({
                 })}
               </p>
             </div>
-            <ChevronRightIcon className={clsx('icon', open && 'rotate-90')} />
           </Disclosure.Button>
           <Transition
             enter="duration-300 ease-out origin-top transform transition"
@@ -44,9 +53,13 @@ const ApplicationPermissions = ({
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Disclosure.Panel className="mb-6">
-              {permissions.map((perm) => (
-                <Permission key={perm.id} {...perm} />
+            <Disclosure.Panel className="mb-6" unmount={false}>
+              {permissions.map((permission) => (
+                <Permission
+                  key={permission.id}
+                  permission={permission}
+                  role={role}
+                />
               ))}
             </Disclosure.Panel>
           </Transition>
