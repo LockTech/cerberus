73be2ac653bf0884df35d1c2d61369f393c66fdb
Ryan <jesselockard99@gmail.com>
2021-09-15 15:08:55 -0400

    updated Permission markup, added mutations, extracted styling

--- web/src/components/Permission/Permission/Permission.tsx
+++ web/src/components/Permission/Permission/Permission.tsx
@@ -1,44 +1,105 @@
-import { useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import { Switch } from '@headlessui/react'
 import clsx from 'clsx'
+import { useCallback, useEffect, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useMutation } from '@apollo/client'
+import { Switch } from '@headlessui/react'
+import { toast } from '@redwoodjs/web/toast'
+
+import { QUERY } from 'src/components/Role/RoleDetailCell'
+
+import { useErrorTranslation } from 'src/hooks/useErrorTranslation'
+
+import { Permission as TPermission, RoleDetailQuery } from 'types/graphql'
+
+import './Permission.css'
+
+export const ADD_MUTATION = gql`
+  mutation RoleAddPermissionMutation($permissionId: ID!, $roleId: ID!) {
+    role: addPermToRole(permissionId: $permissionId, roleId: $roleId) {
+      id
+    }
+  }
+`
+
+export const DEL_MUTATION = gql`
+  mutation RoleDelPermissionMutation($permissionId: ID!, $roleId: ID!) {
+    role: delPermFromRole(permissionId: $permissionId, roleId: $roleId) {
+      id
+    }
+  }
+`
 
 export type PermissionProps = {
-  application: string
-  id: string
-  namespace: string
-  object: string
-  relation: string
+  permission: Pick<
+    TPermission,
+    'application' | 'id' | 'namespace' | 'object' | 'relation'
+  >
+  role: RoleDetailQuery['role']
 }
 
 const Permission = ({
-  application,
-  namespace,
-  object,
-  relation,
+  permission: { application, id: permissionId, namespace, object, relation },
+  role: { id: roleId, permissions },
 }: PermissionProps) => {
   const [active, setActive] = useState(false)
 
   const { t } = useTranslation()
+  const { et } = useErrorTranslation()
+
+  const [add, { loading: addLoading }] = useMutation(ADD_MUTATION, {
+    refetchQueries: [{ query: QUERY, variables: { id: roleId } }],
+    variables: { permissionId, roleId },
+  })
+  const [del, { loading: delLoading }] = useMutation(DEL_MUTATION, {
+    refetchQueries: [{ query: QUERY, variables: { id: roleId } }],
+    variables: { permissionId, roleId },
+  })
+
+  const onChange = useCallback(
+    (state: boolean) => {
+      state &&
+        toast.promise(add(), {
+          loading: t('Permission.add.loading'),
+          success: () => {
+            setActive(state)
+            return t('Permission.add.success')
+          },
+          error: (err) => et(err),
+        })
+      !state &&
+        toast.promise(del(), {
+          loading: t('Permission.del.loading'),
+          success: () => {
+            setActive(state)
+            return t('Permission.del.success')
+          },
+          error: (err) => et(err),
+        })
+    },
+    [add, del, et, t]
+  )
 
   const translationKey = `permissions:${application}.${namespace}#${object}#${relation}`
 
+  useEffect(() => {
+    const match = permissions.find((perm) => perm.id === permissionId)
+    match && setActive(true)
+  }, [permissions, permissionId, setActive])
+
   return (
-    <Switch.Group
-      as="div"
-      className="input-group flex flex-row items-center my-2 px-6 py-4 space-x-10"
-    >
-      <Switch.Label className="cursor-pointer space-y-1">
-        <p className="input-label">{t(`${translationKey}.title`)}</p>
-        <p className="input-hint">{t(`${translationKey}.summary`)}</p>
-      </Switch.Label>
+    <Switch.Group as="div" className="permission">
       <Switch
         checked={active}
-        className={clsx('switch-track', active && 'switch-track-active')}
-        onChange={setActive}
+        className={clsx('permission-switch', active && 'active')}
+        disabled={addLoading || delLoading}
+        onChange={onChange}
       >
         <span aria-hidden="true" className="switch-thumb" />
       </Switch>
+      <Switch.Label className="cursor-pointer space-y-1 w-full">
+        <p className="input-label">{t(`${translationKey}.title`)}</p>
+        <p className="input-hint">{t(`${translationKey}.summary`)}</p>
+      </Switch.Label>
     </Switch.Group>
   )
 }
