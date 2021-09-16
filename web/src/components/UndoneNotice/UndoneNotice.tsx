59a59cb8e4faefa4609dffb1a01e66db81839eb5
Ryan <jesselockard99@gmail.com>
2021-09-16 15:36:53 -0400

    updated aria-disabled -> aria-hidden for UndoneNotice icons

--- web/src/components/UndoneNotice/UndoneNotice.tsx
+++ web/src/components/UndoneNotice/UndoneNotice.tsx
@@ -8,12 +8,12 @@ const UndoneNotice = ({ children }: UndoneNoticeProps) => {
   return (
     <div className="flex flex-row items-center justify-center space-x-3">
       <ExclamationIcon
-        aria-disabled="true"
+        aria-hidden="true"
         className="text-yellow-600 dark:text-yellow-300 h-6 w-6"
       />
       <p className="text font-semibold">{children}</p>
       <ExclamationIcon
-        aria-disabled="true"
+        aria-hidden="true"
         className="text-yellow-600 dark:text-yellow-300 h-6 w-6"
       />
     </div>
