8f0ecf7ef2e1fc5505eaeea804c1e47952ed16ac
Ryan <jesselockard99@gmail.com>
2021-09-16 15:38:16 -0400

    extracted out styling for UndoneNotice

--- web/src/components/UndoneNotice/UndoneNotice.tsx
+++ web/src/components/UndoneNotice/UndoneNotice.tsx
@@ -1,21 +1,17 @@
 import { ExclamationIcon } from '@heroicons/react/solid'
 
+import './UndoneNotice.css'
+
 export type UndoneNoticeProps = {
   children: string
 }
 
 const UndoneNotice = ({ children }: UndoneNoticeProps) => {
   return (
-    <div className="flex flex-row items-center justify-center space-x-3">
-      <ExclamationIcon
-        aria-hidden="true"
-        className="text-yellow-600 dark:text-yellow-300 h-6 w-6"
-      />
+    <div className="undoneNotice">
+      <ExclamationIcon aria-hidden="true" className="icon" />
       <p className="text font-semibold">{children}</p>
-      <ExclamationIcon
-        aria-hidden="true"
-        className="text-yellow-600 dark:text-yellow-300 h-6 w-6"
-      />
+      <ExclamationIcon aria-hidden="true" className="icon" />
     </div>
   )
 }
