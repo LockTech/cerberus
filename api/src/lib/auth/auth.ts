f792af871bd15b637d344579113a93c1f158fb7f
Ryan <jesselockard99@gmail.com>
2021-09-17 11:15:39 -0400

    cleaned up getCurrentUser, added roles, added documentation

--- api/src/lib/auth/auth.ts
+++ api/src/lib/auth/auth.ts
@@ -1,18 +1,29 @@
-import type { Account, Organization } from '@prisma/client'
+import { ValidationError } from 'apollo-server-errors'
 
 import { AccountRemoveAuthFields } from 'src/constants/account'
 
 import { db } from 'src/lib/db'
 
+import type { CurrentUser } from 'types/auth'
 import type { IDInput } from 'types/inputs'
 
+/**
+ * Read an `Account` from the database using the requesting user's `id`, as decryptedfrom their given session.
+ *
+ * If an Account exists and has been `verified`, will return it and its `Organization` and `Roles`.
+ *
+ * @param session
+ * @returns The `Account` belonging to the given `id`, with its `Organization` and `Roles` included.
+ */
 export const getCurrentUser = async (session: IDInput) => {
   let res = await db.account.findFirst({
-    include: { organization: true },
+    include: { organization: true, roles: true },
     where: { id: session.id, verified: true, verifiedAt: { not: null } },
   })
 
-  res = AccountRemoveAuthFields(res) as Account & { organization: Organization }
+  if (res === null) throw new ValidationError('auth-undefined')
+
+  res = AccountRemoveAuthFields(res) as CurrentUser
 
   return res
 }
