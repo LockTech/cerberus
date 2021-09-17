75b6af5e7e99ed91339f2848d9d180d61b3e6518
Ryan <jesselockard99@gmail.com>
2021-09-17 01:42:22 -0400

    updated layout of AuthLayout

--- web/src/layouts/AuthLayout/AuthLayout.tsx
+++ web/src/layouts/AuthLayout/AuthLayout.tsx
@@ -25,19 +25,31 @@ const AuthLayout = ({ children }: AuthLayoutProps) => {
   }, [currentUser, redirectTo])
 
   return (
-    <div className="max-w-md m-auto px-6 sm:px-0 py-6 space-y-6">
-      <a className="dark" href={BrandURL} rel="noreferrer" target="_blank">
-        <BrandBanner className="bg-gray-900 max-w-xs px-4 py-2 rounded-lg" />
-      </a>
-      {children}
-      <div className="flex flex-col items-center space-y-3">
-        <a className="link" href={PrivacyURL} rel="noreferrer" target="_blank">
-          {t('Auth.Layout.privacy')}
-        </a>
-        <a className="link" href={SupportURL} rel="noreferrer" target="_blank">
-          {t('Auth.Layout.support')}
-        </a>
+    <div className="flex flex-col h-full max-w-md m-auto px-6 sm:px-0 py-6">
+      <div className="flex-grow space-y-6">
+        {children}
+        <div className="flex flex-col items-center space-y-3">
+          <a
+            className="link"
+            href={PrivacyURL}
+            rel="noreferrer"
+            target="_blank"
+          >
+            {t('Auth.Layout.privacy')}
+          </a>
+          <a
+            className="link"
+            href={SupportURL}
+            rel="noreferrer"
+            target="_blank"
+          >
+            {t('Auth.Layout.support')}
+          </a>
+        </div>
       </div>
+      <a className="dark" href={BrandURL} rel="noreferrer" target="_blank">
+        <BrandBanner className="bg-gray-900 max-w-[14rem] px-4 py-2 rounded-lg" />
+      </a>
     </div>
   )
 }
