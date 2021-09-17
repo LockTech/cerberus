a9968f8ea260ecf23ebed1ef3ceb1fed47d98826
Ryan <jesselockard99@gmail.com>
2021-09-16 23:18:53 -0400

    cleaned up readFile, added readDir

--- api/src/util/readFile.ts
+++ api/src/util/readFile.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs'
+import { readFileSync, readdirSync } from 'fs'
 
 import { isStr } from 'src/util/asserters'
 
@@ -7,9 +7,15 @@ import { isStr } from 'src/util/asserters'
  *  * 'file-path' - When `path` is not a string
  */
 export const readFile = (path: string) => {
-  if (!isStr(path)) {
-    throw new Error('file-path')
-  }
-
+  if (!isStr(path)) throw new Error('file-path')
   return readFileSync(path).toString()
+}
+
+/**
+ * @throws
+ *  * 'file-path' - When `path` is not a string
+ */
+export const readDir = (path: string) => {
+  if (!isStr(path)) throw new Error('file-path')
+  return readdirSync(path).toString()
 }
