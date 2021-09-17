b7622aae857f44374a7c20b89635adeaafa95f96
Ryan <jesselockard99@gmail.com>
2021-09-17 00:42:14 -0400

    added API and WEB URL variables to web-side

--- web/src/constants/variables.ts
+++ web/src/constants/variables.ts
@@ -5,3 +5,6 @@ export const BrandURL = process.env.BRAND_URL
 export const PrivacyURL = process.env.PRIVACY_URL
 
 export const SupportURL = process.env.SUPPORT_URL
+
+export const ApiURL = process.env.API_URL
+export const WebURL = process.env.WEB_URL
