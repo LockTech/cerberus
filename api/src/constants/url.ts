8cc72066dfde21ade5b5cd450532047c6b2a52d3
Ryan <jesselockard99@gmail.com>
2021-09-18 16:46:29 -0400

    removed API and Web URLs

--- api/src/constants/url.ts
+++
@@ -1,2 +0,0 @@
-export const ApiURL = process.env.API_URL
-export const WebURL = process.env.WEB_URL
--- web/src/constants/variables.ts
+++ web/src/constants/variables.ts
@@ -5,6 +5,3 @@ export const BrandURL = process.env.BRAND_URL
 export const PrivacyURL = process.env.PRIVACY_URL
 
 export const SupportURL = process.env.SUPPORT_URL
-
-export const ApiURL = process.env.API_URL
-export const WebURL = process.env.WEB_URL
