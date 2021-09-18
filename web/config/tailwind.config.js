01d25895f5c1e75a116ec50ac8671bc24459a214
Ryan <jesselockard99@gmail.com>
2021-09-17 22:20:44 -0400

    removed spin-physics from tailwind config

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -1,14 +1,7 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
   presets: [require('@locktech/atomic/preset')],
   purge: ['src/**/*.{js,ts,jsx,tsx,html,css}'],
   theme: {
-    extend: {
-      animation: {
-        'spin-physics': 'spin 1.5s cubic-bezier(.75, .25, .25, .75) infinite',
-      },
-    },
     fontFamily: {
       inter: ['Inter', 'sans-serif'],
     },
