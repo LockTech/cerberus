b26b96a54870e379d5220fb8a3be8c6a77f1420c
Ryan <jesselockard99@gmail.com>
2021-09-19 02:00:26 -0400

    added LocaleEndpoint as constant variable

--- api/src/constants/locales.ts
+++ api/src/constants/locales.ts
@@ -3,3 +3,5 @@ import { resolve } from 'path'
 export const LocaleDirectory = resolve(__dirname, `../../locales`)
 
 export const LocaleLookupRegEx = /^([A-Za-z]*-?[A-Za-z]+\/[A-Za-z]*-?[A-Za-z]+).json$/
+
+export const LocaleEndpoint = '/locales/'
