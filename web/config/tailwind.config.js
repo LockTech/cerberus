4c9065739de8763903a46fb3a7649552f70bd36e
Ryan <jesselockard99@gmail.com>
2021-09-21 21:03:57 -0400

    updated font to extend, not overwrite, defaults

--- web/config/tailwind.config.js
+++ web/config/tailwind.config.js
@@ -2,8 +2,10 @@ module.exports = {
   presets: [require('@locktech/atomic/preset')],
   purge: ['src/**/*.{js,ts,jsx,tsx,html,css}'],
   theme: {
-    fontFamily: {
-      inter: ['Inter', 'sans-serif'],
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
     },
   },
 }
